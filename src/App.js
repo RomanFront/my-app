af118d7c93679f1d9b3963ae4f479734db2e48a3
RomanFront <krylov.r@2wtrade.com>
2021-07-14 22:48:33 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, world!543
+          Hello, world!
         </p>
         <a
           className="App-link"
