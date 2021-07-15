6842c67eb82ecf2ad05d03247587de5f2336bcbc
RomanFront <krylov.r@2wtrade.com>
2021-07-15 16:59:55 +0300

    lesson-16

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,11 +3,18 @@ import s from './Card.module.css'
 
 class Card extends React.Component {
 
+    handleCardClick = () => {
+        console.log('####: it\'s mean', this.props.rus)
+    }
+
     render() {
         const {eng, rus} = this.props;
 
         return (
-            <div className={s.card}>
+            <div 
+                className={s.card}
+                onClick={this.handleCardClick}
+                >
                 <div className={s.cardInner}>
                     <div className={s.cardFront}>
                         {eng}
