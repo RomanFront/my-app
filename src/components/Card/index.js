38107454442d2ae187cefecd1a7043df00aa9dc3
RomanFront <krylov.r@2wtrade.com>
2021-07-15 15:03:10 +0300

    lesson-15

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import s from './Card.module.css'
 
-const Card = ({eng, rus}) => {
-    return (
-        <div className={s.card}>
-            <div className={s.cardInner}>
-                <div className={s.cardFront}>
-                    {eng}
-                </div>
-                <div className={s.cardBack}>
-                    {rus}
+class Card extends React.Component {
+
+    render() {
+        const {eng, rus} = this.props;
+
+        return (
+            <div className={s.card}>
+                <div className={s.cardInner}>
+                    <div className={s.cardFront}>
+                        {eng}
+                    </div>
+                    <div className={s.cardBack}>
+                        {rus}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 export default Card;
