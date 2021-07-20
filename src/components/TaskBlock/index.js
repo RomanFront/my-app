a60d9923a6fbf23b21049386979ecbe5b25e19dc
RomanFront <krylov.r@2wtrade.com>
2021-07-20 10:12:19 +0300

    lesson-24

--- src/components/TaskBlock/index.js
+++ src/components/TaskBlock/index.js
@@ -10,4 +10,17 @@ const TaskBlock = ({children}) => {
     )
 }
 
+const key ='dict.1.1.20210720T063732Z.855f718017eb2d7c.835972bbd96d31e2d66fa391a0af454b284674fd';
+const lang ='en-ru';
+const text = 'time';
+
+const getTranslateWord = async () => {
+    const res = await fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${key}&lang=${lang}&text=${text}`)
+    const body = await res.json();
+
+    return body;
+}
+
+getTranslateWord().then(res => console.log('####: res', res));
+
 export default TaskBlock;
