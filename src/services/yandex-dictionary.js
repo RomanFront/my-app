a7e1a223c39e7caf0a231061869b6fa4aa5e8f50
RomanFront <krylov.r@2wtrade.com>
2021-07-20 11:44:58 +0300

    lesson-26

--- src/services/yandex-dictionary.js
+++ src/services/yandex-dictionary.js
@@ -1,5 +1,4 @@
-const key = 'dict.1.1.20210720T063732Z.855f718017eb2d7c.835972bbd96d31e2d66fa391a0af454b284674fd';
-
+const key = process.env.REACT_APP_YANDEX_API_KEY;
 const getTranslateWord = async (text, lang = 'en-ru') => {
     const res = await fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${key}&lang=${lang}&text=${text}`)
     const body = await res.json();
