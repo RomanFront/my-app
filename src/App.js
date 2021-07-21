2d02c35a4e78568ae1115a90c4d202e52add07dc
RomanFront <krylov.r@2wtrade.com>
2021-07-21 14:34:09 +0300

    lesson-29

--- src/App.js
+++ src/App.js
@@ -1,84 +1,45 @@
 import React, { Component } from 'react';
-import database from './services/firebase';
-import TopMenu from './components/TopMenu';
-import TaskBlock from './components/TaskBlock';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import CardList from './components/CardList';
+import HomePage from './pages/Home';
+import LoginPage from './pages/Login';
+import { fire } from './services/firebase'
+import { Spin } from 'antd';
+import s from './App.module.css';
+
 
 class App extends Component {
   state = {
-    wordArr: [],
+    user: null,
   }
 
   componentDidMount() {
-    database.ref('/cards/').once('value').then(res => {
-      console.log('####: res', res.val());
-      this.setState({
-        wordArr: res.val(),
-      });
+    fire.auth().onAuthStateChanged(user => {
+      console.log('####: user', user)
+      if (user) {
+        this.setState({
+          user,
+        });
+      } else {
+        this.setState({
+          user: false,
+        });
+      }
     });
-  }
-
-  setNewWord = (eng_value, rus_value) => {
-    database.ref('/cards/' + this.state.wordArr.length).set({
-      eng: eng_value,
-      rus: rus_value,
-      id: this.state.wordArr.length,
-    });
-    database.ref('/cards/').once('value').then(res => {
-      console.log('####: res', res.val());
-      this.setState({
-        wordArr: res.val(),
-      });
-    });
-  }
-
-  handleDeletedItem = (id) => {
-    database.ref('/cards/' + id).remove();
-    database.ref('/cards/').once('value').then(res => {
-      console.log('####: res', res.val());
-      this.setState({
-        wordArr: res.val(),
-      });
-    });
-
-
-    // this.setState(({wordArr}) => {
-    //   const idx = wordArr.findIndex(item => item.id === id);
-    //   const newWordArr = [
-    //     ...wordArr.slice(0, idx),
-    //     ...wordArr.slice(idx + 1)
-    //   ]
-    //   return {
-    //     wordArr: newWordArr,
-    //   }
-    // })
-  }
-  handleAddItem = (newWord, isAddDisabled) => {
-    if (!isAddDisabled) {
-      this.setNewWord(newWord.eng, newWord.rus)
-    } else {
-      return
-    }
-    
   }
 
   render() {
-    const { wordArr } = this.state;
+    const { user } = this.state;
+    
+    if (user === null) {
+      return (
+        <div className={s.loader_wrap}>
+          <Spin size="large" />
+        </div>
+      );
+    }
+
     return (
       <>
-        <TaskBlock>
-          <TopMenu />
-          <MainContent>
-            <CardList
-              onAddItem={this.handleAddItem}
-              onDeletedItem={this.handleDeletedItem} 
-              item={wordArr}
-            />
-          </MainContent>
-          <Footer />
-        </TaskBlock>
+        {user ? <HomePage /> : <LoginPage />}
       </>
     )
   }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
+import 'firebase/auth';
 
 
 const firebaseConfig = {
