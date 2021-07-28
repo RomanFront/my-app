72f520aea1c7bd43ee6d2e6e89b39630a8333075
RomanFront <krylov.r@2wtrade.com>
2021-07-28 11:45:08 +0300

    lesson-44

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App'
 import FirebaseContext from './context/firebaseContext';
 import Firebase from './services/firebase';
+import { createStore } from 'redux';
+import rootReducers from './reducers';
 import './index.css';
+import { Provider } from 'react-redux';
+
+const store = new createStore(rootReducers);
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={new Firebase}>
-        <App />
-    </FirebaseContext.Provider>
+    <Provider store={store}>
+        <FirebaseContext.Provider value={new Firebase}>
+            <App />
+        </FirebaseContext.Provider>
+    </Provider>    
     , 
     document.getElementById('root')
     )
